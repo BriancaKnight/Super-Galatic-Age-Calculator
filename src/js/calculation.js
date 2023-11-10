dad7620eaec322742fe8ed380d8bc14df372162c
Brianca Knight <briancaknight@gmail.com>
2023-11-10 14:08:01 -0800

    Pass test 7 to add a difference prototype.

--- src/js/calculation.js
+++ src/js/calculation.js
@@ -1,10 +1,10 @@
 export default class Calculation {
-  constructor(age1, age2){
+  constructor(age1, age2) {
     this.age1 = age1;
     this.age2 = age2;
   }
 
   difference() {
-
+    return this.age1 - this.age2;
   }
 }
