3ba72a8c3e3f88c3389ecd1d77d131660f2c01c5
Brianca Knight <briancaknight@gmail.com>
2023-11-10 14:01:12 -0800

    Pass test 6 creating calculation constructor.

--- __tests__/calculation.test.js
+++ __tests__/calculation.test.js
@@ -2,16 +2,16 @@ import Calculation from "../src/js/calculation.js";
 
 describe('Calculation', () => {
   let newCalculation;
-  let num1 = 100;
-  let num2 = 50;
+  let age1 = 100;
+  let age2 = 50;
 
   beforeEach(() => {
-    newCalculation = new Calculation(num1, num2);
+    newCalculation = new Calculation(age1, age2);
   });
 
   test('It should create a constructor for calculations of two numbers', () => {
-    expect(newCalculation.num1).toEqual(100);
-    expect(newCalculation.num2).toEqual(50);
+    expect(newCalculation.age1).toEqual(100);
+    expect(newCalculation.age2).toEqual(50);
   });
 });
 
--- src/js/calculation.js
+++ src/js/calculation.js
@@ -1,4 +1,6 @@
 export default class Calculation {
-
+  constructor(age1, age2){
+    this.age1 = age1;
+    this.age2 = age2;
+  }
 }
-
