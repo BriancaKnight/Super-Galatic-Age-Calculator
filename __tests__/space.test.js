f528327e924e2d0b0428fc46bc4d50b1fa17b85c
Brianca Knight <briancaknight@gmail.com>
2023-11-10 12:35:45 -0800

    Fail test 2 to create a mercury prototype.

--- __tests__/space.test.js
+++ __tests__/space.test.js
@@ -1,8 +1,17 @@
 import Age from "./../src/js/space.js";
 
 describe('Age', () => {
-  test('It should create a age object with earth years.', () => {
-    const age = new Age(30);
-    expect(age.earth).toEqual(30);
+  let newAge;
+
+  beforeEach(() => {
+    newAge = new Age(100);
   });
+
+  test('It should create a age object with earth years.', () => {
+    expect(newAge.earth).toEqual(100);
+  });
+
+  test('It should create a Merecury prototype to return age in Mercury years.', () => {
+    expect(newAge.mercury).toEqual(24);
+  })
 });
